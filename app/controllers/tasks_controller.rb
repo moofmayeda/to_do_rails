a8f69045fea7d8edf74b0bb8fa14e52c57d7b0f0
moof mayeda & Dylan Peerenboom <student@epicodus.com>
2014-08-18 13:50:35 -0700

    update tasks complete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,13 +6,26 @@ class TasksController <ApplicationController
   end
 
   def create
+    @tasks = Task.all
     @task = Task.new(params[:task])
-    @task.save
-    render('tasks/success.html.erb')
-    # if @task.save
-    #   render('tasks/success.html.erb')
-    # else
-    #   render('tasks/new.html.erb')
-    # end
+    if @task.save
+      render('tasks/success.html.erb')
+    else
+      render('tasks/index.html.erb')
+    end
+  end
+
+  def show
+    @task = Task.find(params[:id])
+    render('tasks/show.html.erb')
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(params[:task])
+      render('/tasks/success.html.erb')
+    else
+      render('/tasks/show.html.erb')
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
 
   match('/', {:via => :get, :to =>'tasks#index'})
   match('/', {:via => :post, :to =>'tasks#create'})
+  match('/:id', {:via => :get, :to => 'tasks#show'})
+  match('/:id', {:via => [:patch, :put], :to => 'tasks#update'})
 end
